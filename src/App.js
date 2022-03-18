85520427a7462c4ba2f386515e697107d064efb4
Raul Veras <verasraul@gmail.com>
2022-03-18 12:43:22 -0400

    added syling to center all components, uppercase function for input and .env file for security

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import './App.css';
+import GetTicker from "./components/GetTicker";
+import TickerSearch from "./components/TickerSearch";
+
 function App() {
   return (
     <div className="App">
-      Hello World
+      Search for Ticker:
+      <TickerSearch  />
+      <GetTicker />
     </div>
   );
 }
